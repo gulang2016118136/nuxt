4d196aedc290471dc93f2890fcb3d2ecae18c8e9
zzz <1296566484@qq.com>
2020-04-07 12:02:59 +0800

    feat:nuxt解决跨域

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+     this is  my first nuxt-app
+     {{response}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name:"index",
+  data(){
+   return{
+    //  response:''
+   }
+  },
+  asyncData ({app}){
+    const reponse= app.$axios.get('/').then(res=>{
+      return {
+        response:res.data
+      }
+    })
+    return reponse
+  }
+}
+</script>
+
+<style>
+
+ 
+</style>
